c9dfb93de3c78f184c2fc253dadda08b5526c40a
Anastasiia Utina <anastasiiaprekrasnaiia@gmail.com>
2022-12-03 22:13:05 +0300

    add junit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,32 +7,61 @@ import org.junit.Test;
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
-    @Test
+    @org.junit.Test
     public void shouldBelowLimit() {
         int amount = 750;
 
         int actual = service.remain(amount);
         int expected = 250;
 
-        Assert.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldLimit() {
         int amount = 1_000;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        Assert.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.Test
     public void shouldAboveLimit() {
         int amount = 1_350;
 
         int actual = service.remain(amount);
         int expected = 650;
-        Assert.assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesThreeWithJupiter() {
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected,actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesTwoWithJupiter() {
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected,actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void testRemainBoundaryValuesSixWithJupiter() {
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1001;
+
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected,actual);
     }
 }
